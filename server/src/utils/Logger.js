5470d31542b1f528e5c410512b0fa4fe84ba1fe4
Avinashs7 <07avinash.s@gmail.com>
2024-12-23 16:39:54 +0530

    vercel has logging issues, fixed

--- server/src/utils/Logger.js
+++ server/src/utils/Logger.js
@@ -2,15 +2,15 @@ const winston = require('winston');
 const fs = require('fs');
 const path = require('path');
 const DailyRotateFile = require('winston-daily-rotate-file');
-const { environment, logDirectory } = require( '../config');
+const { environment, logDirectory } = require('../config');
 
 const { timestamp, label, printf } = winston.format;
+
 let dir = logDirectory;
 if (!dir) dir = path.resolve('logs');
 
-if (!fs.existsSync(dir)) {
-    // Create the directory if it does not exist
-    fs.mkdirSync(dir);
+if (environment !== 'production' && !fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });  
 }
 
 const customColors = {
@@ -29,36 +29,54 @@ const logFormat = printf(({ level, message, label, timestamp }) => {
     return `[${label}] [${level}]: ${message} ${timestamp}`;
 });
 
-const dailyRotateFile = new DailyRotateFile({
-    level: logLevel,
-    filename: dir + '/%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    handleExceptions: true,
-    maxSize: '20m',
-    maxFiles: '14d',
-    format: winston.format.combine(
-        winston.format.errors({ stack: true }),
-        winston.format.timestamp(),
-        timestamp(),
-    ),
-});
+const transports = [
+    new winston.transports.Console({
+        level: logLevel,
+        format: winston.format.combine(
+            winston.format.errors({ stack: true }),
+            label({ label: 'Logging' }),
+            timestamp(),
+            winston.format.colorize(),
+            logFormat,
+        ),
+    }),
+];
 
-const logger=winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            level: logLevel,
-            format: winston.format.combine(
-                winston.format.errors({ stack: true }),
-                label({ label: 'Logging' }),
-                timestamp(),
-                winston.format.colorize(),
-                logFormat,
-            ),
-        }),
-        dailyRotateFile,
-    ],
-    exceptionHandlers: [dailyRotateFile],
+if (environment !== 'production') {
+    const dailyRotateFile = new DailyRotateFile({
+        level: logLevel,
+        filename: dir + '/%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        handleExceptions: true,
+        maxSize: '20m',
+        maxFiles: '14d',
+        format: winston.format.combine(
+            winston.format.errors({ stack: true }),
+            winston.format.timestamp(),
+            timestamp(),
+        ),
+    });
+    transports.push(dailyRotateFile);
+}
+
+const logger = winston.createLogger({
+    transports: transports,
+    exceptionHandlers: [new DailyRotateFile({
+        level: logLevel,
+        filename: dir + '/%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        handleExceptions: true,
+        maxSize: '20m',
+        maxFiles: '14d',
+        format: winston.format.combine(
+            winston.format.errors({ stack: true }),
+            winston.format.timestamp(),
+            timestamp(),
+        ),
+    })],
     exitOnError: false,
 });
-module.exports=logger; 
+
+module.exports = logger;
